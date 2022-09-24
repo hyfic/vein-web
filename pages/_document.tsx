5b5462dd194aa540719dd06f8343e2b7b810fcab
Fadhil <fadhilsaheer@gmail.com>
2022-09-24 07:46:09 +0530

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,35 +11,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang='en'>
-        <Head>
-          <title>Vein</title>
-          <link
-            rel='icon'
-            type='image/png'
-            href='https://raw.githubusercontent.com/octoi/heartbeat/main/website/assets/logo.svg'
-          />
-          <meta name='theme-color' content='#FFFEFE' />
-          <meta name='description' content='Building productive doctors ⚡' />
-          <meta property='og:title' content='Vein' />
-          <meta
-            property='og:description'
-            content='Building productive doctors ⚡'
-          />
-          <meta
-            property='og:image'
-            content='https://raw.githubusercontent.com/octoi/heartbeat/main/website/assets/banner.png'
-          />
-          <meta property='og:type' content='website' />
-          <meta
-            name='twitter:description'
-            content='Building productive doctors ⚡'
-          />
-          <meta
-            name='twitter:image'
-            content='https://raw.githubusercontent.com/octoi/heartbeat/main/website/assets/banner.png'
-          />
-          <meta name='twitter:app:country' content='IN' />
-        </Head>
+        <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
