9dd33cc6488e5db1491dc8382764e8c2501d2456
Fadhil <fadhilsaheer@gmail.com>
2022-09-24 13:49:04 +0530

    print support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import type { NextPage } from 'next';
 import React from 'react';
-import { Button, useColorMode } from '@chakra-ui/react';
+import { Heading } from '@chakra-ui/react';
 import { Layout } from '@/components/layout';
+import { DischargeForm } from '@/components/dischargeForm';
 
 const HomePage: NextPage = () => {
-  const { toggleColorMode } = useColorMode();
-
   return (
     <Layout>
-      <h1 className='text-2xl font-medium underline'>Vein</h1>
-      <p>An application to create discharge sheet fast</p>
-      <Button onClick={toggleColorMode}>Toggle</Button>
+      <Heading>Discharge form</Heading>
+      <DischargeForm />
     </Layout>
   );
 };
