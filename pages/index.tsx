a8eda303afdd1af48b3be5cdd65bfd46d8e0833c
Fadhil <fadhilsaheer@gmail.com>
2022-10-05 08:37:12 +0530

    removed heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next';
 import React from 'react';
-import { Heading } from '@chakra-ui/react';
 import { Layout } from '@/components/layout';
 import { DischargeForm } from '@/components/dischargeForm';
 
 const HomePage: NextPage = () => {
   return (
     <Layout>
-      <Heading>Discharge form</Heading>
       <DischargeForm />
     </Layout>
   );
